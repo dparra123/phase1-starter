b205546179e5b3641dd8b8f40e577f4da7ac7ed2
John H. Hartman <john.hartman@akamai.com>
2021-01-14 17:02:53 -0700

    using tester.h

--- phase1b/tests/test_fork.c
+++ phase1b/tests/test_fork.c
@@ -1,15 +1,31 @@
+#include <stddef.h>
 #include <phase1.h>
 #include <phase1Int.h>
 #include <assert.h>
+#include <tester.h>
 
+// Test forking a process
 static int
 Output(void *arg) 
 {
     char *msg = (char *) arg;
 
-    USLOSS_Console("%s", msg);
+    USLOSS_Console("%s", msg); 
     P1_Quit(11);
     // should not return
+    FAILED(1,0);
+    return 0;
+}
+
+int P6Proc(void *arg){
+    int pid;
+    int rc = P1_Fork("Hello", Output, "Hello World!\n", USLOSS_MIN_STACK, 1, &pid);
+    assert(rc == P1_SUCCESS);
+    int status;
+    rc = P1GetChildStatus(&pid, &status);
+    TEST(rc, P1_SUCCESS);
+    PASSED();
+ 	
     return 0;
 }
 
@@ -20,10 +36,12 @@ startup(int argc, char **argv)
     int rc;
     P1ProcInit();
     USLOSS_Console("startup\n");
-    rc = P1_Fork("Hello", Output, "Hello World!\n", USLOSS_MIN_STACK, 1, 0, &pid);
-    assert(rc == P1_SUCCESS);
+    // fork the first process with priority 6 so it doesn't fail students' check
+    // that priority must be 6 for process with pid = 0
+    rc = P1_Fork("P6Proc", P6Proc, "Hello World!\n", USLOSS_MIN_STACK, 6, &pid);
+    TEST(rc, P1_SUCCESS);
     // P1_Fork should not return
-    assert(0);
+    FAILED(1,0);
 }
 
 void test_setup(int argc, char **argv) {}
