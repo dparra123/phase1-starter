b8ccf10790e5244598d4c435037447b555f83a5b
John H. Hartman <john.hartman@akamai.com>
2021-01-14 17:01:10 -0700

    revised for latest version of the project

--- phase1Int.h
+++ phase1Int.h
@@ -12,23 +12,23 @@
 // Phase 1a
 
 void    P1ContextInit(void);
-int     P1ContextCreate(void (*func)(void *), void *arg, int stacksize, int *cid) CHECKRETURN; 
-int     P1ContextCreate(void (*func)(void *), void *arg, int stacksize, int *cid) CHECKRETURN; 
-int     P1ContextSwitch(int cid) CHECKRETURN;
-int     P1ContextFree(int cid) CHECKRETURN;
-int     P1DisableInterrupts(void) CHECKRETURN;
+int     P1ContextCreate(void (*func)(void *), void *arg, int stacksize, int *cid);
+int     P1ContextSwitch(int cid);
+int     P1ContextFree(int cid);
+int     P1DisableInterrupts(void);
 void    P1EnableInterrupts(void);
 
 // Phase 1b
 
 void    P1ProcInit(void);
-int     P1GetChildStatus(int tag, int *pid, int *status) CHECKRETURN;
-int     P1SetState(int pid, P1_State state, int sid) CHECKRETURN;
+int     P1GetChildStatus(int *cpid, int *status);
+int     P1SetState(int pid, P1_State state, int lid, int vid);
 void    P1Dispatch(int rotate);
 
 // Phase 1c
 
-void    P1SemInit(void);
+void    P1LockInit(void);
+void    P1CondInit(void);
 
 
 #endif
