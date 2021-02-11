eaaaa378fd7d0a9e285520e12f1288873c4370b0
John H. Hartman <john.hartman@akamai.com>
2021-02-11 11:42:27 -0700

    added attribute to check return codes

--- phase1Int.h
+++ phase1Int.h
@@ -9,20 +9,25 @@
 
 #include "phase1.h"
 
+#ifndef CHECKRETURN
+#define CHECKRETURN __attribute__((warn_unused_result))
+#endif
+
+
 // Phase 1a
 
 void    P1ContextInit(void);
-int     P1ContextCreate(void (*func)(void *), void *arg, int stacksize, int *cid);
-int     P1ContextSwitch(int cid);
-int     P1ContextFree(int cid);
-int     P1DisableInterrupts(void);
+int     P1ContextCreate(void (*func)(void *), void *arg, int stacksize, int *cid) CHECKRETURN;
+int     P1ContextSwitch(int cid) CHECKRETURN;
+int     P1ContextFree(int cid) CHECKRETURN;
+int     P1DisableInterrupts(void) CHECKRETURN;
 void    P1EnableInterrupts(void);
 
 // Phase 1b
 
 void    P1ProcInit(void);
-int     P1GetChildStatus(int *cpid, int *status);
-int     P1SetState(int pid, P1_State state, int lid, int vid);
+int     P1GetChildStatus(int *cpid, int *status) CHECKRETURN;
+int     P1SetState(int pid, P1_State state, int lid, int vid) CHECKRETURN;
 void    P1Dispatch(int rotate);
 
 // Phase 1c
